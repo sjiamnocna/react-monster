8c756f75b7c17a7eb17a8d78fbc89f73aadaad84
sjiamnocna <jancikuvmail@gmail.com>
2020-01-27 14:14:21 +0100

    Updates

---
+++ precache-manifest.cce343036e65b29f0a8de525ecdbc191.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc89610a9ed2ccf5b5b7972659a0f9b2",
+    "url": "/react-monster/index.html"
+  },
+  {
+    "revision": "dc24081a7209c8960267",
+    "url": "/react-monster/static/css/main.1b6d5fa5.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/react-monster/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-monster/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dc24081a7209c8960267",
+    "url": "/react-monster/static/js/main.1cc3d1ee.chunk.js"
+  },
+  {
+    "revision": "7a453121113cd2b5f2a4",
+    "url": "/react-monster/static/js/runtime-main.2b46bef7.js"
+  }
+]);
